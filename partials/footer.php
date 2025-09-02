24fdb51e4e59fcc6990e9f36983cb9a0eb7800b2
repgadmrsite-wq <repgad.mr.site@gmail.com>
2025-09-02 22:32:38 +0330

    chore: defer footer scripts

--- partials/footer.php
+++ partials/footer.php
@@ -86,8 +86,8 @@
 
 
 </div>
-<script src="/js/swiper.js" ></script>
-<script src="/js/scripts.js" ></script>
+<script src="/js/swiper.js" defer></script>
+<script src="/js/scripts.js" defer></script>
 <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="fad6f86d-8985-4c3d-808f-e97f7658ec10";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 </body>
 </html>
