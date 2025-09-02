f09ec7b85d3477807c3123f2af4146bdb7b0035d
repgadmrsite-wq <repgad.mr.site@gmail.com>
2025-09-02 15:46:04 +0330

    refactor: modernize color palette

--- partials/header.php
+++ partials/header.php
@@ -16,7 +16,7 @@
     <?php if (!empty($extraHead)) echo strip_tags($extraHead, '<meta><link>'); ?>
     <title><?php echo htmlspecialchars($pageTitle ?? '', ENT_QUOTES, 'UTF-8'); ?></title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/ico">
-    <link rel="stylesheet" href="/dist/style.c5deba9eb2.min.css">
+    <link rel="stylesheet" href="/dist/style.42f23ac7eb.min.css">
 </head>
 <body>
 <script>
