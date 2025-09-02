6446d1f6b37d5525291e8c8b4591c3b6890cb1a9
repgadmrsite-wq <repgad.mr.site@gmail.com>
2025-09-02 13:01:36 +0330

    Add font-display swap to web fonts

--- partials/header.php
+++ partials/header.php
@@ -11,7 +11,7 @@
     <?php if (!empty($extraHead)) echo strip_tags($extraHead, '<meta><link>'); ?>
     <title><?php echo htmlspecialchars($pageTitle ?? '', ENT_QUOTES, 'UTF-8'); ?></title>
     <link rel="shortcut icon" href="/favicon.ico" type="image/ico">
-    <link rel="stylesheet" href="/dist/style.d3ac1c83eb.min.css">
+    <link rel="stylesheet" href="/dist/style.c5deba9eb2.min.css">
 </head>
 <body>
 <script>
